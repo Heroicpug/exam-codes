3f2e45d831611a1b204bff17874be6765d9e19be
Heroicpug <96836624+Heroicpug@users.noreply.github.com>
2022-12-11 19:14:06 +0530

    Create Python function that accepts a string to calculate uppercase and lowercase characters.py

---
+++ Python function that accepts a string to calculate uppercase and lowercase characters.py
@@ -0,0 +1,12 @@
+string= input("Enter the string :")
+lower = upper = other = 0
+for letter in string:
+    if(letter.islower()):
+        lower= lower+1
+    elif(letter.isupper()):
+        upper=upper+1
+    else:
+        other=other+1
+    print("original string",string)
+    print("no.of uppercase character :",upper)
+    print("no.of lowercase character :",lower)
